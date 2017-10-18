2b1f37fecdf58bf5a01933fc5364f6fed0395f38
Dominik Szyja <dery1996@gmail.com>
2017-10-18 21:57:02 +0200

    add sh script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-atom-packages
-This is list of my atom packages included in sh script that I could fastly install 
+This is list of my atom packages included in sh script that I could fastly install everything that I need to work with atom editor.
+
+* last update date 18.10.2017
---
+++ install-kimi-setup-of-atom.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# last update of list: 18.10.2017
+# This is my list of useful atom-packages that I use
+# Basics packages
+apm install open-recent
+apm install todo-show
+apm install minimap
+apm install highlight-selected
+apm install autoclose-html
+apm install pigments
+apm install atom-beautify
+apm install auto-detect-indentation
+apm install platformio-ide-terminal
+apm install autoprefixer
+apm install busy-signal
+apm install git-control
+apm install git-plus
+apm install intentions
+apm install emmet
+apm install script
+
+# Linter packages
+apm install linter
+apm install linter-ui-default
+
+# ES language
+apm install language-babel
+
+# HTML
+apm install linter-htmlhint
+apm install atom-html-preview
+
+# CSS
+apm install linter-sass-lint
+apm install sass-autocompile
+apm install css-snippets
+apm install atom-css-clean
+
+# Themes
+apm install atom-monokai
+apm install monokai-seti
+apm install monokai
+
+apm install atom-material-ui
+apm install atom-material-syntax
+
